ec641cd17abce58cac0f7d50e018528507f19811
root <nwobodofelix047@gmail.com>
2021-08-26 05:59:51 -0700

    alx llp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-  /* print a string */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
