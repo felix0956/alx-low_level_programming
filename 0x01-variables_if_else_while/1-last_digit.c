0bd154dd8f82ec038fec2cfb1d46fabf05617683
root <nwobodofelix047@gmail.com>
2021-08-27 08:07:14 -0700

    alx llp

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,6 +26,6 @@ else if (ld == 0)
 printf("Last digit of %d is %d and is 0\n", n, ld);
 }
 else
-print("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 return (0);
 }
