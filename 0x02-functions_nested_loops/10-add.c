70544933f3febeedd551926d32ba3477218f063f
root <nwobodofelix047@gmail.com>
2021-08-31 13:52:58 -0700

    alx llp

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - check the code for ALX School students.
+ * @a: function parameter
+ * @b: function parameter
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+n = n % 10;
+return (abs(n));
+}
