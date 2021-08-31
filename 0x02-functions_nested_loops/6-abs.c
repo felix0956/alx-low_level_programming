a7fc60754d30c9d1662ef9b5fda513afb34731f2
root <nwobodofelix047@gmail.com>
2021-08-31 13:26:35 -0700

    alx llp

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 
 /**
  * _abs - computes the absolute value of an integer.
- * @int: function parameter
+ * @n: function parameter
  * Return: Always 0.
  */
-int _abs(int n )
+int _abs(int n)
 {
-if (n  >= 0)
+if (n >= 0)
 return (n);
 else
-  return ((n) * -1);
+return ((n) * -1);
 }
