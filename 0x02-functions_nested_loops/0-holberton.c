a928c91c9e3f17d1142343d1db412229e464369d
root <nwobodofelix047@gmail.com>
2021-08-31 04:09:03 -0700

    alx llp

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints Holberton
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+char c[10] = "Holberton";
+for (i = 0; i < 10; i++)
+_putchar(c[i]);
+_putchar('\n');
+return (0);
+}
