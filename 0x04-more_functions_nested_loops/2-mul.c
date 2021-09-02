cadf5fbb8d7ac1bf196d91aec8cf9ab889bf1bdd
root <nwobodofelix047@gmail.com>
2021-09-02 11:02:42 -0700

    alx llp

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,10 @@
 /**
  * mul - check the code
- * @a @b:fn parameter
+ * @a:fn parameter
+ * @b:fn parameter
  * Return: Always 0.
  */
 int mul(int a, int b)
 {
-  return(a * b);
+return (a * b);
 }
