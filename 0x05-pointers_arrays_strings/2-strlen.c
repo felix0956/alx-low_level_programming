2d6c130813190a96540e0851a9426f18a17f7a26
root <nwobodofelix047@gmail.com>
2021-09-05 22:08:05 -0700

    alx pointers and array project

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,10 @@
+#include "string.h"
 /**
  * _strlen - function that returns the length of a string.
- *
+ * @s:func parameter
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
-  int i;
-  int count = 0;
-  for (i = 0; s[i] <= '\0'; i++)
-    count += i;
-  return (count);
+return (strlen(s));
 }
-
