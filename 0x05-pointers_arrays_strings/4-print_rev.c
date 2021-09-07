6bbb0eb0ae22daeec4e75daaa251b59437fa58db
root <nwobodofelix047@gmail.com>
2021-09-07 05:28:55 -0700

    alx pointer and array projects

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,37 +1,19 @@
 #include "main.h"
+#include <string.h>
 
-int _strlen(char *str);
 /**
  *print_rev  - check the code
  *@s: func parameter
  */
 void print_rev(char *s)
 {
-int length = _strlen(s);
-
-for (; length >= 0; length--)
+int i, l;
+l = strlen(s);
+for (i = l; i >= 0; i--)
 {
-if (*(s + length) == '\0')
-length -= 1;
-if (*(s + length == '\0' && *(s + (length + 1)) == '\0')
-break;
-_putchar(*(s + length));
+_putchar(s[i]);
 }
-_putchar(10);
+_putchar('\n');
 }
 
-/**
- * _strlen - writes the character c to stdout
- * @str: The character to print
- * Return: alway return length counted
- */
-int _strlen(char *str)
-{
-int count = 0;
-while (*str != '\0')
-{
-count++;
-str++;
-}
-return (count);
-}
+
